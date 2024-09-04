15a557eb3d6b0dd6f275a9bde07c87ae67688184
vhanh6304 <noobpentakill@gmail.com>
2024-09-04 23:31:50 +0700

    prac 3

--- USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -1,6 +1,7 @@
 package vn.edu.usth.usthweather;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,31 +11,44 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class WeatherActivity extends AppCompatActivity {
 
+    private static final String TAG = "WeatherActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Create a new Fragment to be placed in the activity
+        ForecastFragment firstFragment = new ForecastFragment();
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(
+        R.id.main, firstFragment).commit();
+
+        setContentView(R.layout.activity_weather);
+        Log.i(TAG, "onCreate called");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.i(TAG, "onStart called");
     }
 
     @Override
     protected void onResume() {
         // Always call the superclass method first
         super.onResume();
+        Log.i(TAG, "onResume called");
     }
 
     @Override
     protected void onPause() {
-        // Always call the superclass method first
         super.onPause();
+        Log.i(TAG, "onPause called");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.i(TAG, "onStop called");
     }
 
     @Override
